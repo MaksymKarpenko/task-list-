b4f9139791dcdcb5f2efb06dcf9e14aea21916ea
MaksymKarpenko <karpenko.maxim87gmail.com>
2017-06-29 17:52:00 +0300

    create workflow

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/app.js',
+
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '.app.bundle.js'
+	},
+	module: {
+		rules:[
+			{
+				test: /\.scss$/, 
+				use: ExtractTextPlugin.extract({
+					fallbackLoader: 'style-loader',
+					loader: ['css-loader', 'sass-loader'],
+					publicPath: './build'
+				}) 
+			},
+			
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'build'),
+		compress: true,
+		stats: 'errors-only',
+		open: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'project Demo',
+			template: './src/index.ejs'
+		}),
+
+		new ExtractTextPlugin({
+			filename: 'app.css',
+			disable: false,
+			allChunks: true
+		})
+	]
+}
